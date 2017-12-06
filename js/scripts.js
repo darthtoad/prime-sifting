9903e77480ff4e47710ec2e3b08978e042e83f9b
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2017-12-05 19:59:31 -0800

    change to alert all primes less than or equal to number

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,24 @@ $(document).ready(function(){
   $("#theForm").submit(function() {
     debugger;
     event.preventDefault();
-    var prime = true;
     var number = parseInt($("input#number").val());
+    var primes = [];
     console.log(number);
-    for (var i = 2; i <= (number / 2); i++) {
-      if (number % i === 0) {
-        prime = false;
+    for (var j = 1; j <= number; j++) {
+      if (j === 1) {
+        primes.push(1)
+      } else {
+        var prime = true;
+        for (var i = 2; i <= (j / 2); i++) {
+          if (j % i === 0) {
+            prime = false;
+          }
+        }
+        if (prime) {
+          primes.push(j)
+        }
       }
     }
-    if (prime === true && number !== 1 && number > 0) {
-      alert(number + " is a prime number");
-    } else {
-      alert(number + " is not a prime number");
-    }
+    alert(primes.join(", "));
   })
 })
